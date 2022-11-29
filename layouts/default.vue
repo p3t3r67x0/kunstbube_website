f0237216a1743b4352c64a6c772a1d098a8d02c0
Aurelius Wendelken <webtobesocial@gmail.com>
2022-11-29 16:18:33 +0100

    added some responsive classes to view

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="relative flex flex-col bg-gray-200 text-gray-700 min-h-screen antialiased">
-  <div class="relative">
-    <nav class="absolute top-0 inset-x-0 bg-gray-200 p-3 sm:px-0">
+  <div class="relative -translate-y-3">
+    <nav class="absolute top-0 inset-x-0 z-50 bg-gray-200 p-3 sm:px-0">
       <div class="text-gray-800 flex justify-between mx-auto max-w-screen-xl md:pt-3 md:pb-4">
         <ul class="flex flex-wrap sm:text-xl">
           <li class="mr-6">
@@ -20,7 +20,7 @@
       </div>
     </nav>
 
-    <img src="/images/background-kunstbube.jpg" class="h-screen -translate-y-1/2 object-cover w-full" alt="kunstbube" fetchpriority="high">
+    <img src="/images/background-kunstbube.jpg" class="h-screen z-0 object-cover w-full" alt="kunstbube" fetchpriority="high">
 
     <div class="absolute top-1/2 left-1/2">
       <div class="bg-white -translate-x-1/2 -translate-y-1/2 w-28 sm:w-48 md:w-80">
@@ -32,8 +32,8 @@
     </div>
   </div>
 
-  <div class="grid place-content-center h-18 md:h-48 bg-cyan-500 mb-2 md:mb-4">
-    <p class="text-white text-2xl md:text-5xl">kunstbube. mit der kunst.tube</p>
+  <div class="grid place-content-center md:min-h-48 bg-cyan-500 mb-2 md:mb-4">
+    <p class="text-white text-xl md:text-5xl">kunstbube. mit der kunst.tube</p>
   </div>
 
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 sm:gap-3 md:gap-4 px-2 sm:px-0">
